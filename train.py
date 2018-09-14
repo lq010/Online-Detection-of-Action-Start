40fb6c4d611c995970bdaa45d881a5977851bccf
qing <leiqing010@gmail.com>
2018-09-14 18:45:07 +0200

    .

--- train.py
+++ train.py
@@ -163,13 +163,13 @@ def batch_generator(AS_windows, non_AS_windows, windows_length, batch_size, N_it
             yield X_s, Y
             # yield X_s, X_f, Y
 
-def custom_loss(y_true, y_pred):
-    '''Just another crossentropy'''
-    K.categorical_crossentropy()
-    y_pred = T.clip(y_pred, epsilon, 1.0 - epsilon)
-    y_pred /= y_pred.sum(axis=-1, keepdims=True)
-    cce = T.nnet.categorical_crossentropy(y_pred, y_true)
-    return cce
+# def custom_loss(y_true, y_pred):
+#     '''Just another crossentropy'''
+#     K.categorical_crossentropy()
+#     y_pred = T.clip(y_pred, epsilon, 1.0 - epsilon)
+#     y_pred /= y_pred.sum(axis=-1, keepdims=True)
+#     cce = T.nnet.categorical_crossentropy(y_pred, y_true)
+#     return cce
 
 def main():
     from data import videoPaths as path    
